0de1e470e5f77e7eff715a585bf120043d3fbe01
Nara Andrade <andradesouzanara@gmail.com>
2026-05-23 00:32:28 -0300

    feat: add auth token to api headers if there is a user authenticated. This will enable the user to get, create and delete their own activities

--- baby-diary-frontend/src/hooks/useActivities.ts
+++ baby-diary-frontend/src/hooks/useActivities.ts
@@ -1,20 +1,35 @@
 import { useEffect, useState } from "react";
 import { activitiesService } from "../services/activity.service";
 import { useActivitiesContext } from "./useActivitiesContext";
+import { useAuth0 } from "@auth0/auth0-react";
+import { api } from "../services/api";
 
 export function useActivities() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const { activities, setActivities } = useActivitiesContext();
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     async function fetchActivities() {
       try {
+        if (isAuthenticated) {
+          const token = await getAccessTokenSilently();
+
+          api.interceptors.request.use((config) => {
+            if (token) {
+              config.headers["Authorization"] = `Bearer ${token}`;
+            }
+
+            return config;
+          });
+        }
+
         const data = await activitiesService.getAll();
 
         setActivities(data);
-      } catch (err) {
-        console.log("error", err);
+      } catch (error) {
+        console.error("Error fetching activities:", error);
         setError("Failed to fetch activities");
       } finally {
         setLoading(false);
@@ -22,7 +37,7 @@ export function useActivities() {
     }
 
     fetchActivities();
-  }, [setActivities]);
+  }, [setActivities, isAuthenticated, getAccessTokenSilently]);
 
   return {
     activities,
