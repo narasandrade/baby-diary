10adea84e9556c1ab817dd8fff9f4d830d9a8553
Nara Andrade <andradesouzanara@gmail.com>
2026-06-06 10:59:39 -0300

    feat: display activity's notes and edit notes button

--- baby-diary-frontend/src/components/Activity/ActivityItem/ActivityItem.tsx
+++ baby-diary-frontend/src/components/Activity/ActivityItem/ActivityItem.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import { Card, CardContent, Typography } from "@mui/material";
 import type { Activity } from "../../../../../shared/types/activity";
-import { DeleteActivityButton } from "../DeleteActivityButton/DeleteActivityButton";
+import { formatDate, formatTime } from "../../../utils/date/dateUtils";
+import { DeleteActivityButton, EditActivityNotesButton } from "../index";
 
 export function ActivityItem(activity: Activity) {
   const { type, createdAt } = activity;
-  const activityTime = new Date(createdAt);
+  const [truncateNotes, setTruncateNotes] = useState(true);
 
   const displayDate = formatDate(createdAt);
   const displayTime = formatTime(createdAt);
@@ -14,29 +16,71 @@ export function ActivityItem(activity: Activity) {
       sx={{
         border: 1,
         borderColor: "primary.light",
-        maxHeight: 100,
-        maxWidth: 150,
+        maxHeight: "fit-content",
+        maxWidth: "200px",
         marginBottom: 2,
       }}
     >
       <CardContent
-        sx={{ padding: 1, display: "flex", justifyContent: "space-between" }}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingRight: 1,
+        }}
       >
-        <div>
+        <div style={{ flex: 8 }}>
           <Typography
             gutterBottom
             sx={{
               color: "text.primary",
-              fontSize: 16,
+              fontSize: 18,
+              fontWeight: "bold",
               fontFamily: "Bad Script",
+              textDecoration: "underline",
+              textDecorationStyle: "wavy",
             }}
           >
             {type}
           </Typography>
 
+          {activity.notes && (
+            <Typography
+              gutterBottom
+              sx={{
+                fontSize: 12,
+                background: "aliceblue",
+                color: "text.secondary",
+                border: 1,
+                borderColor: "lightblue",
+                borderRadius: 1,
+                padding: 0.5,
+                marginRight: 1,
+                display: "-webkit-box",
+                WebkitLineClamp: 3,
+                WebkitBoxOrient: "vertical",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                cursor: "pointer",
+              }}
+              onClick={(e) => {
+                const target = e.target as HTMLDivElement;
+
+                if (truncateNotes) {
+                  target.style.webkitLineClamp = "unset";
+                  setTruncateNotes(false);
+                } else {
+                  target.style.webkitLineClamp = "3";
+                  setTruncateNotes(true);
+                }
+              }}
+            >
+              {activity.notes}
+            </Typography>
+          )}
+
           <Typography
             gutterBottom
-            sx={{ color: "text.secondary", fontSize: 14 }}
+            sx={{ color: "text.secondary", fontSize: 13 }}
           >
             {displayDate}
           </Typography>
@@ -45,8 +89,17 @@ export function ActivityItem(activity: Activity) {
             {displayTime}
           </Typography>
         </div>
-        <div>
+        <div
+          style={{
+            flex: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "end",
+          }}
+        >
           <DeleteActivityButton id={activity._id} />
+          <EditActivityNotesButton activity={activity} />
         </div>
       </CardContent>
     </Card>
