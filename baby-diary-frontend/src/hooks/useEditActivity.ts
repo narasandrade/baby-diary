45c1f5a149133ee2b421c84092c8ffa65ec52e38
Nara Andrade <andradesouzanara@gmail.com>
2026-06-06 10:53:54 -0300

    feat: create hook to update activity

---
+++ baby-diary-frontend/src/hooks/useEditActivity.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Activity } from "../../../shared/types/activity";
+import { activitiesService } from "../services/activity.service";
+import { useNotification } from "./useNotification";
+
+const editActivity = async (activity: Activity) => {
+  await activitiesService.update(activity);
+};
+
+export function useEditActivity() {
+  const { setNotify } = useNotification();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: editActivity,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["activities"] });
+
+      setNotify({
+        open: true,
+        message: "Activity edited successfully.",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      setNotify({
+        open: true,
+        message: "Activity editing failed. Please try again.",
+        severity: "error",
+      });
+      console.log("Error editing activity:", error);
+    },
+  });
+
+  return mutation;
+}
