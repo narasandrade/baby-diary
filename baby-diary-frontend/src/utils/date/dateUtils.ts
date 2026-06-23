1361976b5a581628d544a4e34772b4f260505d6a
Nara Andrade <andradesouzanara@gmail.com>
2026-06-22 22:50:22 -0300

    refactor: update format functions

--- baby-diary-frontend/src/utils/date/dateUtils.ts
+++ baby-diary-frontend/src/utils/date/dateUtils.ts
@@ -1,18 +1,19 @@
 export function formatDate(dateString: string): string {
-  const date = new Date(dateString);
+  const date = new Date(dateString).toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
 
-  const day = date.getDate().toString().padStart(2, "0");
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year}`;
+  return date;
 }
 
 export function formatTime(dateString: string): string {
-  const date = new Date(dateString);
+  const time = new Date(dateString).toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
 
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-
-  return `${hours}:${minutes}`;
+  return time;
 }
