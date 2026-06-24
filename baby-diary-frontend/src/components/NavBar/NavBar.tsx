5be41646b3e95668fe12ee8e12142b5739b8a52e
Nara Andrade <andradesouzanara@gmail.com>
2026-06-23 23:26:10 -0300

    feat: update application logo

--- baby-diary-frontend/src/components/NavBar/NavBar.tsx
+++ baby-diary-frontend/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import { useAuth0 } from "@auth0/auth0-react";
-import BabyStrollerIcon from "@/assets/baby-stroller.svg";
+import BabyIcon from "@/assets/baby-icon.svg";
 import { LoginButton, LogoutButton, SignUpButton } from "@/components";
 
 export function NavBar() {
@@ -12,9 +12,13 @@ export function NavBar() {
         <Toolbar>
           <Box
             component="img"
-            src={BabyStrollerIcon}
-            alt="Baby stroller"
-            sx={{ width: 32, height: 32, marginRight: 2 }}
+            src={BabyIcon}
+            alt="Baby icon"
+            sx={{
+              width: 32,
+              height: 32,
+              marginRight: 2,
+            }}
           />
 
           <Typography
